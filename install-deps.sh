0cbfe0df27b4222e4e989552b183a5595ae087d3
Chris Dell <cjdell@gmail.com>
2017-04-21 22:07:08 +0100

    Change script order

--- install-deps.sh
+++ install-deps.sh
@@ -9,14 +9,12 @@ SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 EXTRACT_PATH=$SCRIPT_PATH
 
-cd $EXTRACT_PATH
+if [ -f ~/.config/chromium/Default/Preferences ]; then
+  echo 'Changing Chromium preferences to ask for directory upon saving a download'
+  sed -i 's/"download":{"directory_upgrade":true}/"download":{"directory_upgrade":true,"prompt_for_download":true}/g' ~/.config/chromium/Default/Preferences
+fi
 
 PIP_PACKAGES_PATH=$EXTRACT_PATH/pip-packages
 
 echo "Installing edupy and associated libraries"
 sudo pip3 install $PIP_PACKAGES_PATH/*.*
-
-if [ -f ~/.config/chromium/Default/Preferences ]; then
-  echo 'Changing Chromium preferences to ask for directory upon saving a download'
-  sed -i 's/"download":{"directory_upgrade":true}/"download":{"directory_upgrade":true,"prompt_for_download":true}/g' ~/.config/chromium/Default/Preferences
-fi
