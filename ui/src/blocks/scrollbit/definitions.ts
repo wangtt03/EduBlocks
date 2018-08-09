f0588e287e237eebc3af4c5c4bc5456aa34690e9
joshualowe1002 <josh@edublocks.org>
2018-08-09 13:13:25 +0100

    Fully added scroll:bit

--- ui/src/blocks/scrollbit/definitions.ts
+++ ui/src/blocks/scrollbit/definitions.ts
@@ -24,6 +24,53 @@ export default function define(Blocks: Blockly.BlockDefinitions) {
     }
   };
 
+  Blocks['scrollbit_write'] = {
+    init: function() {
+      this.appendDummyInput()
+          .appendField("write(")
+          .appendField(new Blockly.FieldTextInput("\"Hi!\""), "input")
+          .appendField(")");
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+   this.setTooltip("Write text on scroll:bit");
+   this.setHelpUrl("https://github.com/pimoroni/micropython-scrollbit");
+    }
+  };
+
+  Blocks['scrollbit_draw'] = {
+    init: function() {
+      this.appendDummyInput()
+          .appendField("draw_icon(")
+          .appendField(new Blockly.FieldTextInput("col"), "col")
+          .appendField(", ")
+          .appendField(new Blockly.FieldTextInput("row"), "row")
+          .appendField(", ")
+          .appendField(new Blockly.FieldTextInput("icon"), "icon")
+          .appendField(")");
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+   this.setTooltip("Draw an Icon");
+   this.setHelpUrl("https://github.com/pimoroni/micropython-scrollbit");
+    }
+  };
+
+  Blocks['scrollbit_pixel'] = {
+    init: function() {
+      this.appendDummyInput()
+          .appendField("set_pixel(")
+          .appendField(new Blockly.FieldTextInput("col"), "col")
+          .appendField(", ")
+          .appendField(new Blockly.FieldTextInput("row"), "row")
+          .appendField(", ")
+          .appendField(new Blockly.FieldTextInput("255"), "bright")
+          .appendField(")");
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+   this.setTooltip("Set a pixel");
+   this.setHelpUrl("https://github.com/pimoroni/micropython-scrollbit");
+    }
+  };
+
   Blocks['scrollbit_show'] = {
     init: function() {
       this.appendDummyInput()
@@ -31,6 +78,17 @@ export default function define(Blocks: Blockly.BlockDefinitions) {
       this.setPreviousStatement(true, null);
       this.setNextStatement(true, null);
    this.setTooltip("Show text on scroll:bit");
+   this.setHelpUrl("https://github.com/pimoroni/micropython-scrollbit");
+    }
+  };
+
+  Blocks['scrollbit_clear'] = {
+    init: function() {
+      this.appendDummyInput()
+          .appendField("clear()")
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+   this.setTooltip("Clear text on scroll:bit");
    this.setHelpUrl("https://github.com/pimoroni/micropython-scrollbit");
     }
   };
