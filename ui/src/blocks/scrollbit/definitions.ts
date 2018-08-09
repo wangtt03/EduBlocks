ba658eccb19d9e248528274a0dd35f1c064cb8c0
joshualowe1002 <josh@edublocks.org>
2018-08-09 12:56:15 +0100

    New scroll:bit

--- ui/src/blocks/scrollbit/definitions.ts
+++ ui/src/blocks/scrollbit/definitions.ts
@@ -3,7 +3,7 @@ export default function define(Blocks: Blockly.BlockDefinitions) {
   Blocks['scrollbit_import'] = {
     init: function () {
       this.appendDummyInput()
-        .appendField('import scrollbit');
+        .appendField('from scrollbit import *');
       this.setPreviousStatement(true, null);
       this.setNextStatement(true, null);
       this.setTooltip('Imports the scroll:bit library');
@@ -14,7 +14,7 @@ export default function define(Blocks: Blockly.BlockDefinitions) {
   Blocks['scrollbit_scroll'] = {
     init: function() {
       this.appendDummyInput()
-          .appendField("scrollbit.scroll(")
+          .appendField("scroll(")
           .appendField(new Blockly.FieldTextInput("\"Hello World\""), "input")
           .appendField(")");
       this.setPreviousStatement(true, null);
@@ -27,7 +27,7 @@ export default function define(Blocks: Blockly.BlockDefinitions) {
   Blocks['scrollbit_show'] = {
     init: function() {
       this.appendDummyInput()
-          .appendField("scrollbit.show()")
+          .appendField("show()")
       this.setPreviousStatement(true, null);
       this.setNextStatement(true, null);
    this.setTooltip("Show text on scroll:bit");
--- ui/src/blocks/scrollbit/generators.ts
+++ ui/src/blocks/scrollbit/generators.ts
@@ -9,13 +9,13 @@ export default function define(Python: Blockly.BlockGenerators) {
   Python['scrollbit_scroll'] = function(block) {
     var text_input = block.getFieldValue('input');
     // TODO: Assemble Python into code variable.
-    var code = 'scrollbit.scroll(' +text_input+ ')\n';
+    var code = 'scroll(' +text_input+ ')\n';
     return code;
   };
 
   Python['scrollbit_show'] = function(block) {
     // TODO: Assemble Python into code variable.
-    var code = 'scrollbit.show()\n';
+    var code = 'show()\n';
     return code;
   };
 }
