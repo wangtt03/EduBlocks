ccfbfca225710b5d748b202c369bd5c0a3e5692d
joshualowe1002 <josh@edublocks.org>
2018-08-22 18:45:25 +0100

    Removed micro:bit libs

--- ui/src/blocks/index.ts
+++ ui/src/blocks/index.ts
@@ -22,17 +22,7 @@ const pins = fs.readFileSync(path.join(__dirname, '..', '..', 'src', 'blocks', '
 
 import { Extension } from '../types';
 
-import scrollbitDefs from './scrollbit/definitions';
-import scrollbitGens from './scrollbit/generators';
-scrollbitDefs(Blockly.Blocks);
-scrollbitGens(Blockly.Python as any);
-const scrollbit = fs.readFileSync(path.join(__dirname, '..', '..', 'src', 'blocks', 'scrollbit', 'toolbox.xml'));
 
-import gigglebotDefs from './gigglebot/definitions';
-import gigglebotGens from './gigglebot/generators';
-gigglebotDefs(Blockly.Blocks);
-gigglebotGens(Blockly.Python as any);
-const gigglebot = fs.readFileSync(path.join(__dirname, '..', '..', 'src', 'blocks', 'gigglebot', 'toolbox.xml'));
 
 function getToolBoxXml(extensions: Extension[]) {
   let toolBoxXml = '';
@@ -40,23 +30,8 @@ function getToolBoxXml(extensions: Extension[]) {
   toolBoxXml += '<xml>';
 
   toolBoxXml += basic;
-  toolBoxXml += display;
-  toolBoxXml += buttons;
-  toolBoxXml += accelerometer;
-  toolBoxXml += compass;
-  toolBoxXml += radio;
-  toolBoxXml += speech;
-  toolBoxXml += music;
-  toolBoxXml += neopixel;
+  
   toolBoxXml += pins;
-
-  if (extensions.indexOf('scroll:bit') !== -1) {
-    toolBoxXml += scrollbit;
-  }
-
-  if (extensions.indexOf('gigglebot') !== -1 ) {
-    toolBoxXml += gigglebot;
-  }
 
   toolBoxXml += '</xml>';
 
@@ -64,12 +39,7 @@ function getToolBoxXml(extensions: Extension[]) {
 }
 
 function getBeforeScript(extensions: Extension[]) {
-  if (extensions.indexOf('scroll:bit') !== -1) {
-    return fs.readFileSync(path.join(__dirname, '..', '..', 'src', 'blocks', 'scrollbit', 'scrollbit.py'));
-  }
-  if (extensions.indexOf('gigglebot') !== -1) {
-    return fs.readFileSync(path.join(__dirname, '..', '..', 'src', 'blocks', 'gigglebot', 'gigglebot.py'));
-  }
+  
 }
 
 export {
