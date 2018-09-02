6568853e13c56656ecd789e33698b68c320c8337
joshualowe1002 <15jlow@allhallows.lancs.sch.uk>
2018-09-02 21:50:15 +0100

    done pwm

--- ui/src/blocks/pwm/definitions.ts
+++ ui/src/blocks/pwm/definitions.ts
@@ -1,58 +1,104 @@
 export default function define(Blocks: Blockly.BlockDefinitions) {
-  let digitalcol = "#2C97DF"
+  let pwmcol = "#FF4A60"
   Blocks['import_pulseio'] = {
     init: function () {
       this.appendDummyInput()
         .appendField('import pulseio');
       this.setPreviousStatement(true, null);
       this.setNextStatement(true, null);
-      this.setColour(digitalcol);
+      this.setColour(pwmcol);
       this.setTooltip('Imports the PulseIO library.');
       this.setHelpUrl('http://www.example.com/');
     },
   };
 
-  Blocks['pinconfig'] = {
+  Blocks['import_simpleio'] = {
+    init: function () {
+      this.appendDummyInput()
+        .appendField('import simpleio');
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(pwmcol);
+      this.setTooltip('Imports the PulseIO library.');
+      this.setHelpUrl('http://www.example.com/');
+    },
+  };
+
+  Blocks['pwm_conf'] = {
+    init: function() {
+      this.appendDummyInput()
+          .appendField(new Blockly.FieldVariable("pwm"), "pwm")
+          .appendField(" = pulseio.PWMOut(")
+          .appendField(new Blockly.FieldTextInput("Board.D13"), "pin")
+          .appendField(", duty_cycle=")
+          .appendField(new Blockly.FieldTextInput(""), "dc")
+          .appendField(", frequency = ")
+          .appendField(new Blockly.FieldTextInput("50"), "fr")
+          .appendField(")");
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(pwmcol);
+   this.setTooltip("");
+   this.setHelpUrl("");
+    }
+  };
+
+  Blocks['pwm_conf2'] = {
     init: function() {
       this.appendDummyInput()
           .appendField(new Blockly.FieldVariable("pin"), "pin")
-          .appendField(" = DigitalInOut(")
+          .appendField(" = pulseio.PWMOut(")
           .appendField(new Blockly.FieldTextInput(""), "pinno")
           .appendField(")");
       this.setPreviousStatement(true, null);
       this.setNextStatement(true, null);
-      this.setColour(digitalcol);
+      this.setColour(pwmcol);
    this.setTooltip("");
    this.setHelpUrl("");
     }
   };
 
-  Blocks['pindirection'] = {
+  Blocks['simple_tone'] = {
+    init: function() {
+      this.appendDummyInput()
+          .appendField("simple.tone(")
+          .appendField(new Blockly.FieldTextInput(""), "pinno")
+          .appendField(")");
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(pwmcol);
+   this.setTooltip("");
+   this.setHelpUrl("");
+    }
+  };
+
+  Blocks['pwm_dc'] = {
     init: function() {
       this.appendDummyInput()
           .appendField(new Blockly.FieldVariable("pin"), "pin")
-          .appendField(".direction = ")
+          .appendField(".duty_cycle = ")
           .appendField(new Blockly.FieldTextInput(""), "direction");
       this.setPreviousStatement(true, null);
       this.setNextStatement(true, null);
-      this.setColour(digitalcol);
+      this.setColour(pwmcol);
    this.setTooltip("");
    this.setHelpUrl("");
     }
   };
 
-  Blocks['pinvalue'] = {
+  Blocks['pwm_freq'] = {
     init: function() {
       this.appendDummyInput()
           .appendField(new Blockly.FieldVariable("pin"), "pin")
-          .appendField(".value = ")
-          .appendField(new Blockly.FieldDropdown([["True","True"], ["False","False"]]), "value");
+          .appendField(".frequency = ")
+          .appendField(new Blockly.FieldTextInput(""), "direction");
       this.setPreviousStatement(true, null);
       this.setNextStatement(true, null);
-      this.setColour(digitalcol);
+      this.setColour(pwmcol);
    this.setTooltip("");
    this.setHelpUrl("");
     }
   };
+
 
 }
--- ui/src/blocks/pwm/generators.ts
+++ ui/src/blocks/pwm/generators.ts
@@ -1,33 +1,56 @@
 export default function define(Python: Blockly.BlockGenerators) {
 
 
-  Python['import_digitalio'] = function (block) {
-    const code = 'from digitalio import *\n';
+  Python['import_pulseio'] = function (block) {
+    const code = 'from pulseio import *\n';
     return code;
   };
 
-  Python['pinconfig'] = function(block) {
+  Python['import_simpleio'] = function (block) {
+    const code = 'from simpleio import *\n';
+    return code;
+  };
+
+  Python['pwm_conf'] = function(block) {
+    var variable_pwm = Blockly.Python.variableDB_.getName(block.getFieldValue('pwm'), Blockly.Variables.NAME_TYPE);
+    var text_pin = block.getFieldValue('pin');
+    var text_dc = block.getFieldValue('dc');
+    var text_fr = block.getFieldValue('fr');
+    // TODO: Assemble Python into code variable.
+    var code = variable_pwm+ ' = pulseio.PWMOut(' +text_pin+ ', duty_cycle=' +text_dc+ ', frequency=' +text_fr+ ')\n';
+    return code;
+  };
+
+  Python['pwm_conf2'] = function(block) {
     var variable_pin = Blockly.Python.variableDB_.getName(block.getFieldValue('pin'), Blockly.Variables.NAME_TYPE);
     var text_pinno = block.getFieldValue('pinno');
     // TODO: Assemble Python into code variable.
     var code = variable_pin+ ' = DigitalInOut(' +text_pinno+ ')\n';
     return code;
   };
+  
+  Python['simple_tone'] = function(block) {
+    var text_pinno = block.getFieldValue('pinno');
+    // TODO: Assemble Python into code variable.
+    var code = 'simpleio.tone(' +text_pinno+ ')\n';
+    return code;
+  };
 
-  Python['pindirection'] = function(block) {
+  Python['pwm_dc'] = function(block) {
     var variable_pin = Blockly.Python.variableDB_.getName(block.getFieldValue('pin'), Blockly.Variables.NAME_TYPE);
     var text_direction = block.getFieldValue('direction');
     // TODO: Assemble Python into code variable.
-    var code = variable_pin+ '.direction = ' +text_direction+ '\n';
+    var code = variable_pin+ '.duty_cycle = ' +text_direction+ '\n';
     return code;
   };
 
-  Python['pinvalue'] = function(block) {
+  Python['pwm_freq'] = function(block) {
     var variable_pin = Blockly.Python.variableDB_.getName(block.getFieldValue('pin'), Blockly.Variables.NAME_TYPE);
-    var dropdown_value = block.getFieldValue('value');
+    var text_direction = block.getFieldValue('direction');
     // TODO: Assemble Python into code variable.
-    var code = variable_pin+ '.value = ' +dropdown_value+ '\n';
+    var code = variable_pin+ '.frequency = ' +text_direction+ '\n';
     return code;
   };
+
 
 }
