caa78a6396109101c9479e23f58e348da306432c
joshualowe1002 <josh@edublocks.org>
2018-12-30 18:39:03 +0000

    Added border around alert modal

--- ui/src/views/AlertModal.tsx
+++ ui/src/views/AlertModal.tsx
@@ -27,16 +27,16 @@ export default class AlertModal<T extends AlertModalOption> extends Component<Al
       <div class='modal'>
         <input id='modal_1' type='checkbox' disabled={true} checked={this.props.visible} />
         <label for='modal_1' class='overlay'></label>
-        <article>
+        <article class="AlertModel__container"> 
           <header class="SelectModal__header">
             <h3>{this.props.title}</h3>
             <a class='SelectModal__close close' onClick={() => this.props.onButtonClick('close')}>&times;</a>
           </header>
 
-          <section class='content'>
+          <section class='SelectModel__content'>
             {this.props.text}
           </section>
-          <footer>
+          <footer class="alertFooter">
             <label for="modal_1" class="button dangerous" onClick={() => this.props.onButtonClick('close')}>
               Close
             </label>
