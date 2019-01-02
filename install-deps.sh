6c2e54bc1588bd8ff652b92374d411eb3e426d7b
joshualowe1002 <josh@edublocks.org>
2019-01-02 23:15:38 +0000

    Fixed missing logo

--- install-deps.sh
+++ install-deps.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-
 if [ $(whoami) == 'root' ]; then
-  echo 'Please do not run me as root'
+  echo "Please do not run me as root"
   exit 1
 fi
 
@@ -10,9 +9,9 @@ SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 EXTRACT_PATH=$SCRIPT_PATH
 
 if [ -f ~/.config/chromium/Default/Preferences ]; then
-  echo 'Changing Chromium preferences to ask for directory upon saving a download'
+  echo "Changing Chromium preferences to ask for directory upon saving a download \n"
   sed -i 's/"download":{"directory_upgrade":true}/"download":{"directory_upgrade":true,"prompt_for_download":true}/g' ~/.config/chromium/Default/Preferences
 fi
 
-echo "Installing edupy and associated libraries"
-sudo pip3 install edupy python-sonic blinkt explorerhat 'ipython==6.0.0'
+echo "Installing EduBlocks Python Libraries \n"
+sudo pip3 install python-sonic blinkt explorerhat 'ipython==6.0.0' cs20-microbitio
