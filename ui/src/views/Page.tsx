e39b2bed4dd9b8afcc174d77b1ec07a8ea2a591a
joshualowe1002 <joshualowe1002@gmail.com>
2019-04-18 17:39:21 +0100

    removed an s

--- ui/src/views/Page.tsx
+++ ui/src/views/Page.tsx
@@ -220,7 +220,7 @@ export default class Page extends Component<Props, State> {
       let ip: string | null = null;
 
       if (window.location.protocol === 'https:') {
-        alert('Need to switch to HTTPS to access Raspberry Pi mode...');
+        alert('Need to switch to HTTP to access Raspberry Pi mode...');
         window.location.protocol = 'http:';
         return;
       }
