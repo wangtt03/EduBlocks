4cc015b8e85ecb139e4ec86692c41878bf98ad03
joshualowe1002 <josh@edublocks.org>
2019-08-02 22:48:36 +0100

    pls work

--- ui/src/blocks/index.ts
+++ ui/src/blocks/index.ts
@@ -52,9 +52,6 @@ export async function getToolBoxXml(extensions: Extension[]) {
     (await import('./web/random/generators')).default(Blockly.Python as any);
     toolBoxXml += fs.readFileSync(path.join(__dirname, '..', '..', 'src', 'blocks', 'web', 'random', 'toolbox.xml'));
 
-    (await import('./web/processing/definitions')).default(Blockly.Blocks);
-    (await import('./web/processing/generators')).default(Blockly.Python as any);
-    toolBoxXml += fs.readFileSync(path.join(__dirname, '..', '..', 'src', 'blocks', 'web', 'processing', 'toolbox.xml'));
   }
 
   if (extensions.indexOf('CircuitPython General') !== -1) {
